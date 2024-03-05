8d2cb90c3cea348f4936b81766acd097c274e8d1
4gora <rosarioalessandro03@gmail.com>
2024-03-04 21:46:38 -0300

    Alterações na lógica adicionadas

--- projeto/index.js
+++ projeto/index.js
@@ -1,4 +1,27 @@
-let pedra = document.getElementById('pedra')
-let papel = document.getElementById('papel')
-let tesoura = document.getElementById('tesoura')
+let botoesJogador = document.querySelectorAll('#playerDiv button')
 
+function escolhaComputador() {
+    let lista_escolhas = ["Pedra", "Papel", "Tesoura"];
+    let escolha = lista_escolhas[Math.floor(Math.random() * lista_escolhas.length)]
+    return escolha
+}
+
+botoesJogador.forEach(function(botao) {
+    botao.addEventListener('click', function() {
+        let escolhaComputadorElemento = document.getElementById('computerChoice')
+        escolhaComputadorElemento.textContent = escolhaComputador()
+    })
+})
+
+tesoura.addEventListener("click", function() {
+    alert("você clicou na tesoura!")
+})
+pedra.addEventListener("click", function() {
+    alert("você clicou na pedra!")
+})
+papel.addEventListener("click", function() {
+    alert("você clicou no papel!")
+})
+
+console.log(escolha)
+console.log(escolhaComputadorElemento)
